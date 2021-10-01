873691cea99181dea60761720214647c32c4edfa
Sebastian Deisel <seb.deisel@posteo.de>
2021-10-01 17:31:12 +0200

    remove pin on destroy

--- portalMarker/portalMarker.cs
+++ portalMarker/portalMarker.cs
@@ -2,7 +2,8 @@ using BepInEx;
 using HarmonyLib;
 using System.Reflection;
 using UnityEngine;
- 
+
+
 namespace PortalMarker
 {
     [BepInPlugin("sebdei.portalmarker", "Marks all portals on the minimap when renaming and using them", "1.0.0")]
@@ -14,9 +15,9 @@ namespace PortalMarker
             Harmony harmony = new Harmony("sebdei.portalmarker");
             harmony.PatchAll();
         }
- 
+
         [HarmonyPatch(typeof(TeleportWorld), "SetText")]
-        static class MarkOnEditName
+        static class PinOnEditName
         {
             static void Postfix(string text, TeleportWorld __instance)
             {
@@ -28,47 +29,73 @@ namespace PortalMarker
                 }
             }
         }
- 
+
         [HarmonyPatch(typeof(TeleportWorld), "Teleport")]
-        static class MarkOnUse
+        static class PinOnUse
         {
             static void Postfix(TeleportWorld __instance, ZNetView ___m_nview)
             {
                 string text = __instance.GetText();
- 
+
                 if (text != null)
                 {
                     ReplaceOrigin(__instance, text);
                     ReplaceTarget(___m_nview, text);
                 }
             }
- 
+
             private static void ReplaceTarget(ZNetView ___m_nview, string text)
             {
                 ZDOID zDOID = ___m_nview.GetZDO().GetZDOID("target");
                 ZDO zDO = zDOID == null ? null : ZDOMan.instance.GetZDO(zDOID);
                 Vector3? targetPosition = zDO?.GetPosition();
- 
+
                 if (targetPosition.HasValue)
                 {
                     ReplacePin(targetPosition.Value, text);
                 }
             }
- 
+
             private static void ReplaceOrigin(TeleportWorld __instance, string text)
             {
                 Vector3? originPosition = __instance?.gameObject?.transform?.position;
- 
+
                 if (originPosition.HasValue)
                 {
                     ReplacePin(originPosition.Value, text);
                 }
             }
         }
- 
-        private static void AddPin(Vector3 position, string text)
+
+        [HarmonyPatch(typeof(Piece), "OnDestroy")]
+        static class UnpinOnDestroy
         {
-            Minimap.instance.AddPin(position, Minimap.PinType.Icon4, text, true, false, 0L);
+            static void Prefix(Piece __instance)
+            {
+                GameObject gameObject = __instance.gameObject;
+
+                if (gameObject != null && gameObject.name == "portal_wood")
+                {
+                    TeleportWorld teleportWorld = gameObject.GetComponent<TeleportWorld>();
+                    Vector3? position = teleportWorld?.transform?.position;
+
+                    if (position.HasValue)
+                    {
+                        RemovePin(position.Value);
+                    }
+                }
+            }
+        }
+
+        private static void RemovePin(Vector3 position)
+        {
+            var flags = BindingFlags.Public | BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Static;
+            Minimap.PinData pin = (Minimap.PinData)typeof(Minimap).GetMethod("GetClosestPin", flags).Invoke(Minimap.instance, new object[] { position, 1L });
+
+            if (pin != null)
+            {
+                Minimap.instance.RemovePin(pin);
+            }
         }
 
         private static void ReplacePin(Vector3 position, string text)
@@ -76,16 +103,10 @@ namespace PortalMarker
             RemovePin(position);
             AddPin(position, text);
         }
-        
-        private static void RemovePin(Vector3 position)
+
+        private static void AddPin(Vector3 position, string text)
         {
-            var flags = BindingFlags.Public | BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Static;
-            Minimap.PinData pin = (Minimap.PinData)typeof(Minimap).GetMethod("GetClosestPin", flags).Invoke(Minimap.instance, new object[] { position, 1L });
- 
-            if (pin != null)
-            {
-                Minimap.instance.RemovePin(pin);
-            }
+            Minimap.instance.AddPin(position, Minimap.PinType.Icon4, text, true, false, 0L);
         }
     }
 }
